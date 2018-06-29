88fbfb14e68bd59e68252589a31b95f54330fa6e
Balázs Terényi <balazs.terenyi@gmail.com>
2018-06-29 10:04:14 +0200

    promise

--- promise.js
+++ promise.js
@@ -2,14 +2,14 @@ var M, V, C;
 
 
 M = (function () {
-    
+
     return {
-        
+
     };
 })();
 
 V = (function () {
-    
+
     return {
         nevBeolvasas: function () {
             return document.getElementById('nev').value;
@@ -18,7 +18,7 @@ V = (function () {
         bevitelimezoTorles: function () {
             document.getElementById('nev').value = '';
         },
-        
+
         kiIras: function (talalat) {
             document.getElementById('eredmeny').innerHTML = 'name=' + talalat.results[0].nameFull + '<br />' + 'serial=' + talalat.results[0].uid + '<br />' + 'email=' + talalat.results[0].mail[0] + '<br />' + 'notesmail=' + talalat.results[0].notesEmail;
         },
@@ -26,33 +26,49 @@ V = (function () {
 })();
 
 C = (function (MObj, VObj) {
+    function makeAjaxCall(url) {
+        var promiseObj = new Promise(function (resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
+            xhr.send();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        console.log("xhr done successfully");
+                        resolve(JSON.parse(xhr.responseText));
+                    } else {
+                        reject(xhr.status);
+                        console.log("xhr failed");
+                    }
+                } else {
+                    console.log("xhr processing going on");
+                }
+            }
+            console.log("request sent succesfully");
+        });
+        return promiseObj;
+    }
+
+    //function processUserDetailsResponse(userData) { console.log("render user details", userData); }
+
+    function processBpResponse(searchResult) {
+        console.log("render BP response", searchResult);
+        VObj.kiIras(searchResult);
+    }
+
+    function errorHandler(statusCode) { console.log("failed with status", statusCode); }
+
     function bluepagesKereses() {
         var keresettNev = VObj.nevBeolvasas();
         if (keresettNev != '') {
-            var promiseObj = new Promise(function(resolve, reject){
-                var xhttp = new XMLHttpRequest();
-                xhttp.open('GET', 'https://w3-services1.w3-969.ibm.com/myw3/unified-profile/v1/search/user?query=' + keresettNev + '&rows=1&searchConfig=optimized_search', true);
-                xhttp.send();
-                xhttp.onreadystatechange = function () {
-                    if (xhttp.readyState === 4){
-                        if (xhttp.status === 200){                                                       
-                            resolve(VObj.kiIras(JSON.parse(this.responseText)));
-                            console.log("xhr done successfully");
-                        } else {
-                           reject(xhttp.status);
-                           console.log("xhr failed");
-                        }
-                     } else {
-                        console.log("xhr processing going on");
-                     }
-                }  
-            });
+            var bpUrl = 'https://w3-services1.w3-969.ibm.com/myw3/unified-profile/v1/search/user?query=' + keresettNev + '&rows=1&searchConfig=optimized_search';
+            makeAjaxCall(bpUrl).then(processBpResponse, errorHandler);
         } else {
             alert('Nem írtál be nevet!');
         }
         //VObj.bevitelimezoTorles();
     }
-    
+
     return {
         init: function () {
             document.getElementById('keresgomb').addEventListener('click', bluepagesKereses);
